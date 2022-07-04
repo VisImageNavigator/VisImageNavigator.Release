2905ab4de308481b4d9543b308b0f12bfaed1c31
VisImageNavigator <visimagenavigator@gmail.com>
2022-07-04 16:56:22 -0400

    V3.1

---
+++ public/dataset/README.md
@@ -0,0 +1,12 @@
+The paperData_3.0.3.csv is from https://sites.google.com/site/vispubdata/home.
+
+If you use this dataset  above we would appreciate a citation. 
+Petra Isenberg, Florian Heimerl, Steffen Koch, Tobias Isenberg, Panpan Xu, Chad Stolper, Michael Sedlmair, Jian Chen, Torsten Möller, and John Stasko. vispubdata.org: A Metadata Collection about IEEE Visualization (VIS) Publications. IEEE Transactions on Visualization and Computer Graphics, 23(9):2199–2206, September 2017. (doi: 10.1109/TVCG.2016.2615308) 
+
+The vispubData30_updated_3.0.8.csv is generated from https://ieee-dataport.org/open-access/ieee-vis-figures-and-tables-image-dataset.
+
+If you use this dataset above we would appreciate a citation. 
+Jian Chen, Meng Ling, Rui Li, Petra Isenberg, Tobias Isenberg, Michael Sedlmair, Torsten Möller, Robert S. Laramee, Han-Wei Shen, Katharina Wünsche, and Qiru Wang. VIS30K: A Collection of Figures and Tables from IEEE Visualization Conference Publications. IEEE Transactions on Visualization and Computer Graphics (2021)
+
+
+
---
+++ public/javascripts/constant.js
@@ -0,0 +1,40 @@
+/*
+* VisImageNavigator V3.1 (https://github.com/VisImageNavigator/VisImageNavigator.Release)
+* Copyright 2020-2022 Rui Li, Department of Computer Science and Engineering, The Ohio State University.
+* Licensed under MIT (https://github.com/VisImageNavigator/VisImageNavigator.Release/blob/main/LICENSE)
+* To cite this work: Jian Chen, Meng Ling, Rui Li, Petra Isenberg, Tobias Isenberg, Michael Sedlmair, Torsten Möller, Robert S. Laramee, Han-Wei Shen, Katharina Wünsche, and Qiru Wang. VIS30K: A Collection of Figures and Tables from IEEE Visualization Conference Publications. IEEE Transactions on Visualization and Computer Graphics (2021).
+*/
+
+const iconsUrlDic = {
+    'bar': 'public/images/icons/bar-0.png',
+    'point': 'public/images/icons/scatter-0.png',
+    'line': 'public/images/icons/line-0.png',
+    'node-link': 'public/images/icons/nodelinkcir-0.png',
+    'area': 'public/images/icons/area-0.png',
+    'surface-volume': 'public/images/icons/surface-0.png',
+    'grid': 'public/images/icons/matrix-0.png',
+    'glyph': 'public/images/icons/glyph-0.png',
+    'schematic': 'public/images/icons/flowchart-0.png',
+    'gui': 'public/images/icons/GUI-0.png',
+    'pattern': 'public/images/icons/continuous-0.png',
+    'text': 'public/images/icons/text-0.png',
+    'color': 'public/images/icons/color-0.png',
+    'others': 'public/images/icons/other-0.png',
+};
+
+const iconsAcUrlDic = {
+    'bar': 'public/images/icons/bar-1.png',
+    'point': 'public/images/icons/scatter-1.png',
+    'line': 'public/images/icons/line-1.png',
+    'node-link': 'public/images/icons/nodelinkcir-1.png',
+    'area': 'public/images/icons/area-1.png',
+    'surface-volume': 'public/images/icons/surface-1.png',
+    'grid': 'public/images/icons/matrix-1.png',
+    'glyph': 'public/images/icons/glyph-1.png',
+    'schematic': 'public/images/icons/flowchart-1.png',
+    'gui': 'public/images/icons/GUI-1.png',
+    'pattern': 'public/images/icons/continuous-1.png',
+    'text': 'public/images/icons/text-1.png',
+    'color': 'public/images/icons/color-1.png',
+    'others': 'public/images/icons/other-1.png',
+};
